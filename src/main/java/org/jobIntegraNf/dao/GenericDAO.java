a5eee1e4a4ea7cfe34e81f97f7ddb4778e948ab6
gxstdev <gxstdev@gmail.com>
2025-09-15 23:26:07 -0300

    criando entidade nf e dao

---
+++ src/main/java/org/jobIntegraNf/dao/GenericDAO.java
@@ -0,0 +1,61 @@
+package org.jobIntegraNf.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+import java.util.List;
+
+public class GenericDAO<T> {
+    private final Class<T> clazz;
+    private final EntityManager em;
+
+    public GenericDAO(Class<T> clazz, EntityManager em) {
+        this.clazz = clazz;
+        this.em = em;
+    }
+
+    public void salvar(T entity) {
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.persist(entity);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public T buscarPorId(Object id) {
+        return em.find(clazz, id);
+    }
+
+    public List<T> buscarTodos() {
+        String sql = "SELECT * FROM TB_NF";
+        return em.createNativeQuery(sql, this.clazz).getResultList();
+    }
+
+    public void atualizar(T entity) {
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.merge(entity);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void deletar(T entity) {
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.remove(em.contains(entity) ? entity : em.merge(entity));
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/jobIntegraNf/model/TbNF.java
@@ -0,0 +1,76 @@
+package org.jobIntegraNf.model;
+
+import jakarta.persistence.*;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "TB_NF")
+public class TbNF {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ger_seq_nf")
+    @SequenceGenerator(name = "ger_seq_nf", sequenceName = "seq_cd_nf", allocationSize = 1)
+    @Column(name = "CD_NF")
+    private Long codigoNF;
+
+    @Column(name = "CD_STATUS", nullable = false)
+    private Long codigoStatus;
+
+    @Column(name = "DT_EMISSAO", columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private OffsetDateTime dataEmissao;
+
+    @Column(name = "DT_PROCESSAMENTO", columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private OffsetDateTime dataProcessamento;
+
+    public TbNF() {
+    }
+
+    public TbNF(Long codigoNF, Long codigoStatus, OffsetDateTime dataEmissao, OffsetDateTime dataProcessamento) {
+        this.codigoNF = codigoNF;
+        this.codigoStatus = codigoStatus;
+        this.dataEmissao = dataEmissao;
+        this.dataProcessamento = dataProcessamento;
+    }
+
+    public Long getCodigoNF() {
+        return codigoNF;
+    }
+
+    public void setCodigoNF(Long codigoNF) {
+        this.codigoNF = codigoNF;
+    }
+
+    public Long getCodigoStatus() {
+        return codigoStatus;
+    }
+
+    public void setCodigoStatus(Long codigoStatus) {
+        this.codigoStatus = codigoStatus;
+    }
+
+    public OffsetDateTime getDataEmissao() {
+        return dataEmissao;
+    }
+
+    public void setDataEmissao(OffsetDateTime dataEmissao) {
+        this.dataEmissao = dataEmissao;
+    }
+
+    public OffsetDateTime getDataProcessamento() {
+        return dataProcessamento;
+    }
+
+    public void setDataProcessamento(OffsetDateTime dataProcessamento) {
+        this.dataProcessamento = dataProcessamento;
+    }
+
+    @Override
+    public String toString() {
+        return  "TbNF |" +
+                "codigoNF |" + getCodigoNF() +
+                "codigoStatus |" + getCodigoStatus() +
+                "dataEmissao |" + getDataEmissao() +
+                "dataProcessamento |" + getDataProcessamento();
+    }
+}
+
