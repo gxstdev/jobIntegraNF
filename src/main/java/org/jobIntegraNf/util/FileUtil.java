ae5b8e495c26de03696555aa2f550e3a881c7eec
gxstdev <gxstdev@gmail.com>
2025-10-05 23:24:32 -0300

    Refactor FileUtil and remove directory constants

--- src/main/java/org/jobIntegraNf/util/FileUtil.java
+++ src/main/java/org/jobIntegraNf/util/FileUtil.java
@@ -1,78 +1,19 @@
 package org.jobIntegraNf.util;
 
-import org.jobIntegraNf.dao.ParametroSistemaDAO;
-import org.jobIntegraNf.enums.Parametros;
 import org.jobIntegraNf.exception.FileException;
-import org.jobIntegraNf.model.NotaFiscal;
-import org.jobIntegraNf.model.ParametroSistema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
 
 public class FileUtil {
-    private static final ParametroSistemaDAO parametroSistemaDAO = new ParametroSistemaDAO(ParametroSistema.class);
-
-    public static final String DIRETORIO_NF_PENDENTES = getDirPendentes();
-
-    public static final String DIRETORIO_NF_PROCESSADAS = getDirProcessadas();
-
-    public static final String DIRETORIO_NFS_EXPURGADAS = getDirExpurgadas();
-
     private static final Logger log = LoggerFactory.getLogger(FileUtil.class);
 
-
-    public static void gerarNFTxt(NotaFiscal nf) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(DIRETORIO_NF_PENDENTES + gerarNomeArquivo(nf)))) {
-            bw.write(nf.toString());
-        } catch (IOException e) {
-            String errorMsg = String.format("Erro ao gerar arquivo TXT da NF: %d. Caused by %s",
-                    nf.getCodigoNF(), e.getMessage());
-            throw new FileException(errorMsg + e.getMessage());
-        }
-    }
-
-    private static String gerarNomeArquivo(NotaFiscal nf) {
-        return String.format("nf_%d.txt", nf.getCodigoNF());
-    }
-
-    public static String getDirPendentes() {
-        return parametroSistemaDAO.findByDescricaoParametro(Parametros.DIRETORIO_NFS_PENDENTES.getDescricaoParametro());
-    }
-
-    public static String getDirProcessadas() {
-        return parametroSistemaDAO.findByDescricaoParametro(Parametros.DIRETORIO_NFS_PROCESSADA.getDescricaoParametro());
-    }
-
-    public static String getDirExpurgadas() {
-        return parametroSistemaDAO.findByDescricaoParametro(Parametros.DIRETORIO_NFS_EXPURGADAS.getDescricaoParametro());
-    }
-
-    public static List<File> getNFsTxtPendentes() {
-        File[] files = new File(DIRETORIO_NF_PENDENTES).listFiles(File::isFile);
-        return ((files != null && files.length > 0) ? new ArrayList<>(Arrays.asList(files)) : new ArrayList<>());
-    }
-
-    public static List<File> getNFsTxtProcessadas() {
-        File[] files = new File(DIRETORIO_NF_PROCESSADAS).listFiles(File::isFile);
-        return ((files != null && files.length > 0) ? new ArrayList<>(Arrays.asList(files)) : new ArrayList<>());
-    }
-
-    public static List<File> getNFsTxtExpurgadas() {
-        File[] files = new File(DIRETORIO_NFS_EXPURGADAS).listFiles(File::isFile);
-        return ((files != null && files.length > 0) ? new ArrayList<>(Arrays.asList(files)) : new ArrayList<>());
-    }
     public static void moverArquivos(List<File> arquivos, String destino) {
         try {
             for (File arquivo : arquivos) {
@@ -81,42 +22,9 @@ public class FileUtil {
                 log.info("Movendo arquivo: {} - para: {}", arquivo.getName(), destino);
             }
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new FileException("Não foi possível mover arquivos para o diretório: " + destino);
+            log.error("Não foi possível mover arquivos para o diretório: " + destino, e);
+            throw new FileException("Não foi possível mover arquivos para o diretório: " + destino, e);
         }
-    }
-
-    public static List<Long> extrairCodigosNFs(List<File> arquivosParaProcessar) {
-        return validarNomeArquivos(arquivosParaProcessar)
-                .stream()
-                .map(arq -> Long.valueOf(arq.getName().split("_")[1].replace(".txt", "")))
-                .collect(Collectors.toList());
-    }
-
-    public static List<File> validarNomeArquivos(List<File> arquivosParaProcessar) {
-        List<File> arquivosInvalidos = new ArrayList<>();
-
-        List<File> arquivosValidos = arquivosParaProcessar.stream().filter(arquivo -> {
-                    String[] splNomeArquivo = arquivo.getName().split("_");
-                    if (splNomeArquivo.length >= 2) {
-                        String cdNF = splNomeArquivo[1].replace(".txt", "");
-                        try {
-                            Long.parseLong(cdNF);
-                            return true;
-                        } catch (NumberFormatException e) {
-                            arquivosInvalidos.add(arquivo);
-                            return false;
-                        }
-                    }
-                    arquivosInvalidos.add(arquivo);
-                    return false;
-                }
-        ).collect(Collectors.toList());
-
-        if (!arquivosInvalidos.isEmpty()) {
-            log.warn("Os arquivos {} possuem nomes inválidos, não podem ser processados", arquivosInvalidos);
-        }
-        return arquivosValidos;
     }
 
     /**
@@ -135,8 +43,8 @@ public class FileUtil {
             arquivos.subList(0, limite).clear();
             return batch;
         } catch (Exception e) {
-            e.printStackTrace();
-            throw new FileException("Não foi possível gerar batch de arquivos" + e);
+            log.error("Não foi possível gerar batch de arquivos", e);
+            throw new FileException("Não foi possível gerar batch de arquivos", e);
         }
     }
 
@@ -147,8 +55,8 @@ public class FileUtil {
                 arq.delete();
             }
         }catch (Exception e) {
-            e.printStackTrace();
-            throw new FileException("Não foi possível deletar arquivos" + e);
+            log.error("Não foi possível deletar arquivos", e);
+            throw new FileException("Não foi possível deletar arquivos", e);
         }
 
     }
--- src/test/java/FileUtilsTest.java
+++ src/test/java/FileUtilsTest.java
@@ -5,37 +5,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FileUtilsTest {
-    @Test
-    void testExtrairCodigosNFs() {
-        File f1 = new File("nf_1001.txt");
-        File f2 = new File("nf_1002.txt");
-        List<File> arquivos = List.of(f1, f2);
-
-        List<Long> codigos = FileUtil.extrairCodigosNFs(arquivos);
-
-        assertEquals(2, codigos.size());
-        assertTrue(codigos.contains(1001L));
-        assertTrue(codigos.contains(1002L));
-    }
-    @Test
-    void testExtrairCodigosNFsComArquivoInvalido() {
-        File f1 = new File("nf_1001.txt");
-        File f2 = new File("arquivo_invalido.txt");
-        List<File> arquivos = List.of(f1, f2);
-
-        List<Long> codigos = FileUtil.extrairCodigosNFs(arquivos);
-
-        // deve ignorar o inválido
-        assertEquals(1, codigos.size());
-        assertTrue(codigos.contains(1001L));
-    }
-
     @Test
     void testMoverArquivos() throws IOException {
         Path srcDir = Files.createTempDirectory("pendentes");
@@ -48,22 +22,5 @@ public class FileUtilsTest {
 
         assertFalse(f1.exists());
         assertTrue(Files.exists(destDir.resolve("nf_1001.txt")));
-    }
-
-    @Test
-    void testValidarNomeArquivos(){
-        File f1 = new File("nf__1001.txt");
-        File f2 = new File("nf_invalido.txt");
-        File f3 = new File("nf_4560.txt");
-
-        List<File> arquivos = List.of(f1, f2, f3);
-
-        List<File> arquivosValidos = FileUtil.validarNomeArquivos(arquivos);
-        assertEquals(1, arquivosValidos.size());
-
-        arquivos = new ArrayList<>();
-        arquivosValidos = FileUtil.validarNomeArquivos(arquivos);
-
-        assertEquals(0, arquivosValidos.size());
     }
 }
